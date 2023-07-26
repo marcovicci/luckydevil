93b11909085bff39b7a125923473687409c1b11e
marcovicci <hi@zelle.zone>
2023-07-26 13:04:04 +1200

    github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://marcovicci.github.io',
+  base: '/luckydevil',
   integrations: [mdx(), sitemap(), tailwind()]
 });
